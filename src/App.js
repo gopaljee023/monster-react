4b2aeda155e1588036ff375aad490e2d41ecb3c2
gopaljee <gopaljee023@gmail.com>
2020-03-25 17:29:58 +0530

    use of state with array element, and rendering of array element

--- src/App.js
+++ src/App.js
@@ -6,48 +6,38 @@ class MyClass extends React.Component{
   
   constructor(){
     super();
-    this.state = { 
-     string: 'GOPAL',
-     string2:'Madhu'
-    };
+    //Arrays
+    this.state ={ 
+      monsters:[
+        { 
+          name: 'GOPAL',
+          id:'id1'
+        },
+        {
+          name:"Madhu",
+           id:'id2'
+        },
+        {
+          name:"Akhilesh",
+          id:'id3'
+        }
+      ]
+    }
   }
+
+ 
    
  
-  shoot = () =>{
-    alert("Great Shot!");
-  }
-
-  changeName = ()=>{
-    this.setState(
-      {string:"Gopal Jee",
-      string2:'MadhuMita'})
-  }
-
-   changeName2(){
-    this.setState({string:"Gopal Jee"})
-  }
   render(){
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-          
-            <h1>{this.state.string}</h1>
-            <h1>{this.state.string2}</h1>
-          </p>
-          <button onClick={this.changeName2()}>click</button>
-          <button onClick={this.changeName}>click</button>
-         <button onClick={()=>this.setState({string:"Gopal Jee"})}>lambda click</button>
-         <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {
+            
+          this.state.monsters.map(
+              mon=><h1 key={mon.id}>{mon.name}</h1>
+                )
+            
+        }
       </div>
     );
   }
