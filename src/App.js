eea4d3c5531ff516c397316e63fe16793bb9057e
gopaljee <gopaljee023@gmail.com>
2020-03-25 17:04:16 +0530

    how to use setState()

--- src/App.js
+++ src/App.js
@@ -7,27 +7,38 @@ class MyClass extends React.Component{
   constructor(){
     super();
     this.state = { 
-     string: 'GOPAL'
+     string: 'GOPAL',
+     string2:'Madhu'
     };
   }
    
  
-  shoot= () =>{
+  shoot = () =>{
     alert("Great Shot!");
   }
 
+  changeName = ()=>{
+    this.setState(
+      {string:"Gopal Jee",
+      string2:'MadhuMita'})
+  }
 
+   changeName2(){
+    this.setState({string:"Gopal Jee"})
+  }
   render(){
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-           {this.state.string}
-           <h1>Learning state use</h1>
+          
+            <h1>{this.state.string}</h1>
+            <h1>{this.state.string2}</h1>
           </p>
-          <button onClick={this.shoot} >click</button>
-          <button onClick={()=>{alert("from lambda")}}>click2</button>
+          <button onClick={this.changeName2()}>click</button>
+          <button onClick={this.changeName}>click</button>
+         <button onClick={()=>this.setState({string:"Gopal Jee"})}>lambda click</button>
          <a
             className="App-link"
             href="https://reactjs.org"
