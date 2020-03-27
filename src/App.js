dfa65f487c40ac9decc005aac97a87dcdbfcfcc1
gopaljee <gopaljee023@gmail.com>
2020-03-27 16:05:52 +0530

    use of filter and includes to get filtered monsters

--- src/App.js
+++ src/App.js
@@ -26,12 +26,16 @@ class MyClass extends React.Component{
    
  
   render(){
+    const {monsters,searchField} = this.state;
+    const filteredMonsters = monsters.filter(monster=>
+      monster.name.toLowerCase().includes( searchField.toLowerCase()))
+
     return (
       <div className="App">
         <input type="search" placeholder="search monsters"
          onChange={e=> this.setState({searchField:e.target.value}, ()=>console.log(this.state))}
         />
-        <CardList monsters={this.state.monsters}/>
+        <CardList monsters={filteredMonsters}/>
        </div>
     )
   }
