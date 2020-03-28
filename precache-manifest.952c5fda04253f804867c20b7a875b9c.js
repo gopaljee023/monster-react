6d6bd86f630aca55108a7e8c4657da584724e159
gopaljee <gopaljee023@gmail.com>
2020-03-28 14:03:36 +0530

    Updates

---
+++ precache-manifest.952c5fda04253f804867c20b7a875b9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e98aa08fa6bc954c8c9a710b8f62838",
+    "url": "/github.io/react_static/index.html"
+  },
+  {
+    "revision": "88ed08cfc5e521c1fa24",
+    "url": "/github.io/react_static/static/css/main.92b23eb6.chunk.css"
+  },
+  {
+    "revision": "6cd801de0dc6dd9870fc",
+    "url": "/github.io/react_static/static/js/2.7aaa3d17.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github.io/react_static/static/js/2.7aaa3d17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88ed08cfc5e521c1fa24",
+    "url": "/github.io/react_static/static/js/main.053a910e.chunk.js"
+  },
+  {
+    "revision": "74da1d68f02591fd2793",
+    "url": "/github.io/react_static/static/js/runtime-main.f1bf6790.js"
+  }
+]);
